d70f5d23e05ad9380e07f76d55f51950c3012695
Jason Lehmann <jasonlehmann@Jasons-MacBook-Pro.local>
2015-07-22 08:58:15 -0400

    Initial Commit

---
+++ FoodBounty1.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodBounty1.1
+//
+//  Created by Jason Lehmann on 7/22/15.
+//  Copyright (c) 2015 USA Swiss Assistance Program. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
